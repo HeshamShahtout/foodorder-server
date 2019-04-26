df1fac570d1052dbdd669f7861049a74a3cb15ea
Hesham Shahtout <heshamshahtout@gmail.com>
2019-04-26 18:50:03 +0200

    Added update and delete categories

---
+++ app/src/main/java/com/example/eatitserver/ViewHolder/MenuViewHolder.java
@@ -0,0 +1,53 @@
+package com.example.eatitserver.ViewHolder;
+
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.eatitserver.Common.Common;
+import com.example.eatitserver.ItemClickListener;
+import com.example.eatitserver.R;
+
+
+public class MenuViewHolder extends RecyclerView.ViewHolder implements
+        View.OnClickListener,
+        View.OnCreateContextMenuListener
+{
+
+    public TextView txtMenuName;
+    public ImageView imageView;
+
+    private ItemClickListener itemClickListner;
+
+
+    public MenuViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        txtMenuName = itemView.findViewById(R.id.menu_name);
+        imageView = itemView.findViewById(R.id.menu_image);
+
+        itemView.setOnClickListener(this);
+        itemView.setOnCreateContextMenuListener(this);
+    }
+
+    public void setItemClickListner(com.example.eatitserver.ItemClickListener itemClickListner) {
+        this.itemClickListner = itemClickListner;
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListner.onClick(v, getAdapterPosition(),false);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("Select the action");
+
+        menu.add(0,0,getAdapterPosition(), Common.UPDATE);
+        menu.add(0,0,getAdapterPosition(), Common.DELETE);
+    }
+}
